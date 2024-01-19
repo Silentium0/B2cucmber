a06731bcd5f2d167a5ab5e1997fa88c54f946fd2
sergii <davygorasergii@gmail.com>
2024-01-18 18:30:24 -0800

    Map Of String list

--- src/test/java/com/loop/pages/OrderSmartBear.java
+++
@@ -1,4 +0,0 @@
-package com.loop.pages;
-
-public class OrderSmartBear extends SmartBearBasePage {
-}
--- src/test/java/com/loop/runner/CukesRunner.java
+++ src/test/java/com/loop/runner/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "json:target/json-report/json-report.json"},
         features = "src/test/resources/features",
         glue = "com/loop/step_definitions",
-        dryRun = true,
-        tags =  "@SmartBears" //  or @smoke1 or @smoke2 or @smoke3"   // if you want to run all  of them
+        dryRun = false,
+        tags =  "@mapList" //  or @smoke1 or @smoke2 or @smoke3"   // if you want to run all  of them
         //monochrome = true
 )
 
